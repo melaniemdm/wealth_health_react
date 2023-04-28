1f148214f3fc769209c83c8fa2e20a42c124f704
melaniemdm <melanieborja.pro@gmail.com>
2023-04-28 17:02:03 +0200

    refacto(js) commentaires

--- src/pages/EmployeeList.js
+++ src/pages/EmployeeList.js
@@ -1,91 +1,92 @@
-
-import MaterialTable from 'material-table';
-import { ThemeProvider, createTheme } from '@mui/material';
-import { forwardRef, React } from 'react';
-import AddBox from '@material-ui/icons/AddBox';
-import ArrowDownward from '@material-ui/icons/ArrowDownward';
-import Check from '@material-ui/icons/Check';
-import ChevronLeft from '@material-ui/icons/ChevronLeft';
-import ChevronRight from '@material-ui/icons/ChevronRight';
-import Clear from '@material-ui/icons/Clear';
-import DeleteOutline from '@material-ui/icons/DeleteOutline';
-import Edit from '@material-ui/icons/Edit';
-import FilterList from '@material-ui/icons/FilterList';
-import FirstPage from '@material-ui/icons/FirstPage';
-import LastPage from '@material-ui/icons/LastPage';
-import Remove from '@material-ui/icons/Remove';
-import SaveAlt from '@material-ui/icons/SaveAlt';
-import Search from '@material-ui/icons/Search';
-import ViewColumn from '@material-ui/icons/ViewColumn';
-import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-
+import MaterialTable from "material-table";
+import { ThemeProvider, createTheme } from "@mui/material";
+import { forwardRef, React } from "react";
+import AddBox from "@material-ui/icons/AddBox";
+import ArrowDownward from "@material-ui/icons/ArrowDownward";
+import Check from "@material-ui/icons/Check";
+import ChevronLeft from "@material-ui/icons/ChevronLeft";
+import ChevronRight from "@material-ui/icons/ChevronRight";
+import Clear from "@material-ui/icons/Clear";
+import DeleteOutline from "@material-ui/icons/DeleteOutline";
+import Edit from "@material-ui/icons/Edit";
+import FilterList from "@material-ui/icons/FilterList";
+import FirstPage from "@material-ui/icons/FirstPage";
+import LastPage from "@material-ui/icons/LastPage";
+import Remove from "@material-ui/icons/Remove";
+import SaveAlt from "@material-ui/icons/SaveAlt";
+import Search from "@material-ui/icons/Search";
+import ViewColumn from "@material-ui/icons/ViewColumn";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 //icons de l'app
 const tableIcons = {
-    Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
-    Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
-    Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
-    Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
-    DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
-    Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
-    Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
-    Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
-    FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
-    LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
-    NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
-    PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
-    ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
-    Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
-    SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
-    ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
-    ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
-  };
-  
- 
+  Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
+  Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
+  Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+  Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
+  DetailPanel: forwardRef((props, ref) => (
+    <ChevronRight {...props} ref={ref} />
+  )),
+  Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
+  Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
+  Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
+  FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
+  LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
+  NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+  PreviousPage: forwardRef((props, ref) => (
+    <ChevronLeft {...props} ref={ref} />
+  )),
+  ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+  Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
+  SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
+  ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
+  ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />),
+};
 
-export default function EmployeeList(){
- //recuperation du store
-const employees =useSelector((state)=>state.allEmployees)  
-// copie des employées du store dans l'objet data car le state est immuable et le composant MaterialTable doit modifier les objects du tableau de données
-const data = employees.map(employee => ({ ...employee }));
+export default function EmployeeList() {
+  //recuperation du store
+  const employees = useSelector((state) => state.allEmployees);
+  // copie des employées du store dans l'objet data car le state est immuable et le composant MaterialTable doit modifier les objects du tableau de données
+  const data = employees.map((employee) => ({ ...employee }));
 
-      //creation du thème pour le tableau
-    const defaultMaterialTheme = createTheme();
+  //creation du thème pour le tableau
+  const defaultMaterialTheme = createTheme();
 
-    return <div>
- <div id="employee-div" className="container">
-            <h1>Current Employees</h1>
-            <table id="employee-table" className="display"></table>
-           
-        </div>
-        {/* plugin de thème material UI */}
-        <ThemeProvider theme={defaultMaterialTheme}>
-          {/* plugin du tableau liste des employers */} 
+  return (
+    <div>
+      <div id="employee-div" className="container">
+        <h1>Current Employees</h1>
+        <table id="employee-table" className="display"></table>
+      </div>
+      {/* plugin de thème material UI */}
+      <ThemeProvider theme={defaultMaterialTheme}>
+        {/* plugin du tableau liste des employers */}
         <MaterialTable
-        icons={tableIcons}
-        title=" "
-        columns={[
-          { title: 'First Name', field: 'firstName' },
-          { title: 'Last Name', field: 'lastName' },
-          { title: 'Start date', field: 'startDate' },
-          { title: 'Department', field: 'department' },
-          { title: 'Date of Birth', field: 'dateBirth' },
-          { title: 'Street', field: 'street' },
-          { title: 'City', field: 'city' },
-          { title: 'State', field: 'stateCode' },
-          { title: 'Zip code', field: 'zipCode' },
-        ]}
-        // props la liste des employers
-        data={data}    
-        options={{
-            search: true
+          icons={tableIcons}
+          title=" "
+          columns={[
+            { title: "First Name", field: "firstName" },
+            { title: "Last Name", field: "lastName" },
+            { title: "Start date", field: "startDate" },
+            { title: "Department", field: "department" },
+            { title: "Date of Birth", field: "dateBirth" },
+            { title: "Street", field: "street" },
+            { title: "City", field: "city" },
+            { title: "State", field: "stateCode" },
+            { title: "Zip code", field: "zipCode" },
+          ]}
+          // props la liste des employers
+          data={data}
+          options={{
+            search: true,
           }}
-         
-      />  
-  
+        />
       </ThemeProvider>
 
-      <Link to="/Formulaire" id="home">Home</Link>
+      <Link to="/Formulaire" id="home">
+        Home
+      </Link>
     </div>
+  );
 }
--- src/pages/Formulaire.js
+++ src/pages/Formulaire.js
@@ -1,360 +1,389 @@
-import { HashLink as Link } from 'react-router-hash-link';
+import { HashLink as Link } from "react-router-hash-link";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import { useState } from 'react';
-import Dropdown from 'react-dropdown';
-import 'react-dropdown/style.css';
-import {  useDispatch } from 'react-redux';
-import { addEmployee } from '../store/redux';
-import { SimpleModal, openModal } from 'mdm-simple-modal';
+import { useState } from "react";
+import Dropdown from "react-dropdown";
+import "react-dropdown/style.css";
+import { useDispatch } from "react-redux";
+import { addEmployee } from "../store/redux";
+import { SimpleModal, openModal } from "mdm-simple-modal";
 
+export default function Formulaire() {
+  // création de la fonction pour le dispatch dans la liste des nouveaux employés
+  const dispatch = useDispatch();
+  // state des champs du formulaire
+  const [startDate, setStartDate] = useState(new Date());
+  const departments = [
+    "Sales",
+    "Marketing",
+    "Engineering",
+    "Human Resources",
+    "Legal",
+  ];
+  const [selectedDepartment, setSelectedDepartment] = useState(departments[0]);
+  const states = [
+    {
+      name: "Alabama",
+      abbreviation: "AL",
+    },
+    {
+      name: "Alaska",
+      abbreviation: "AK",
+    },
+    {
+      name: "American Samoa",
+      abbreviation: "AS",
+    },
+    {
+      name: "Arizona",
+      abbreviation: "AZ",
+    },
+    {
+      name: "Arkansas",
+      abbreviation: "AR",
+    },
+    {
+      name: "California",
+      abbreviation: "CA",
+    },
+    {
+      name: "Colorado",
+      abbreviation: "CO",
+    },
+    {
+      name: "Connecticut",
+      abbreviation: "CT",
+    },
+    {
+      name: "Delaware",
+      abbreviation: "DE",
+    },
+    {
+      name: "District Of Columbia",
+      abbreviation: "DC",
+    },
+    {
+      name: "Federated States Of Micronesia",
+      abbreviation: "FM",
+    },
+    {
+      name: "Florida",
+      abbreviation: "FL",
+    },
+    {
+      name: "Georgia",
+      abbreviation: "GA",
+    },
+    {
+      name: "Guam",
+      abbreviation: "GU",
+    },
+    {
+      name: "Hawaii",
+      abbreviation: "HI",
+    },
+    {
+      name: "Idaho",
+      abbreviation: "ID",
+    },
+    {
+      name: "Illinois",
+      abbreviation: "IL",
+    },
+    {
+      name: "Indiana",
+      abbreviation: "IN",
+    },
+    {
+      name: "Iowa",
+      abbreviation: "IA",
+    },
+    {
+      name: "Kansas",
+      abbreviation: "KS",
+    },
+    {
+      name: "Kentucky",
+      abbreviation: "KY",
+    },
+    {
+      name: "Louisiana",
+      abbreviation: "LA",
+    },
+    {
+      name: "Maine",
+      abbreviation: "ME",
+    },
+    {
+      name: "Marshall Islands",
+      abbreviation: "MH",
+    },
+    {
+      name: "Maryland",
+      abbreviation: "MD",
+    },
+    {
+      name: "Massachusetts",
+      abbreviation: "MA",
+    },
+    {
+      name: "Michigan",
+      abbreviation: "MI",
+    },
+    {
+      name: "Minnesota",
+      abbreviation: "MN",
+    },
+    {
+      name: "Mississippi",
+      abbreviation: "MS",
+    },
+    {
+      name: "Missouri",
+      abbreviation: "MO",
+    },
+    {
+      name: "Montana",
+      abbreviation: "MT",
+    },
+    {
+      name: "Nebraska",
+      abbreviation: "NE",
+    },
+    {
+      name: "Nevada",
+      abbreviation: "NV",
+    },
+    {
+      name: "New Hampshire",
+      abbreviation: "NH",
+    },
+    {
+      name: "New Jersey",
+      abbreviation: "NJ",
+    },
+    {
+      name: "New Mexico",
+      abbreviation: "NM",
+    },
+    {
+      name: "New York",
+      abbreviation: "NY",
+    },
+    {
+      name: "North Carolina",
+      abbreviation: "NC",
+    },
+    {
+      name: "North Dakota",
+      abbreviation: "ND",
+    },
+    {
+      name: "Northern Mariana Islands",
+      abbreviation: "MP",
+    },
+    {
+      name: "Ohio",
+      abbreviation: "OH",
+    },
+    {
+      name: "Oklahoma",
+      abbreviation: "OK",
+    },
+    {
+      name: "Oregon",
+      abbreviation: "OR",
+    },
+    {
+      name: "Palau",
+      abbreviation: "PW",
+    },
+    {
+      name: "Pennsylvania",
+      abbreviation: "PA",
+    },
+    {
+      name: "Puerto Rico",
+      abbreviation: "PR",
+    },
+    {
+      name: "Rhode Island",
+      abbreviation: "RI",
+    },
+    {
+      name: "South Carolina",
+      abbreviation: "SC",
+    },
+    {
+      name: "South Dakota",
+      abbreviation: "SD",
+    },
+    {
+      name: "Tennessee",
+      abbreviation: "TN",
+    },
+    {
+      name: "Texas",
+      abbreviation: "TX",
+    },
+    {
+      name: "Utah",
+      abbreviation: "UT",
+    },
+    {
+      name: "Vermont",
+      abbreviation: "VT",
+    },
+    {
+      name: "Virgin Islands",
+      abbreviation: "VI",
+    },
+    {
+      name: "Virginia",
+      abbreviation: "VA",
+    },
+    {
+      name: "Washington",
+      abbreviation: "WA",
+    },
+    {
+      name: "West Virginia",
+      abbreviation: "WV",
+    },
+    {
+      name: "Wisconsin",
+      abbreviation: "WI",
+    },
+    {
+      name: "Wyoming",
+      abbreviation: "WY",
+    },
+  ];
+  const [selectedState, setSelectedState] = useState(states[0].name);
+  async function validEmployee() {
+    // recupere les value des input
+    const firstName = document.querySelector("#firstName").value;
+    const lastName = document.querySelector("#lastName").value;
+    const dateBirth = document.querySelector("#dateBirth").value;
+    const startDate = document.querySelector("#startDate").value;
+    const street = document.querySelector("#street").value;
+    const stateCode = selectedState;
+    const city = document.querySelector("#city").value;
+    const zipCode = document.querySelector("#zipCode").value;
+    const department = selectedDepartment;
 
+    // payload de l'action
+    const newEmployeeInfo = {
+      firstName: firstName,
+      lastName: lastName,
+      startDate: startDate,
+      department: department,
+      dateBirth: dateBirth,
+      street: street,
+      city: city,
+      stateCode: stateCode,
+      zipCode: zipCode,
+    };
+    // appel de la function use dispatch avec en paramètre l'action et le nouvel employé
+    dispatch(addEmployee(newEmployeeInfo));
+  }
+  //recupere l'option de department
+  function onSelectDepartment(option) {
+    const seletedOption = { selected: option };
+    setSelectedDepartment(seletedOption.selected.value);
+  }
+  //recupere l'option de State
+  function onSelectState(option) {
+    const selecState = { selected: option };
+    setSelectedState(selecState.selected.value);
+  }
 
-export default function Formulaire(){
-// création de la fonction pour le dispatch dans la liste des nouveaux employés
-    const dispatch = useDispatch();
-// state des champs du formulaire
-    const [startDate, setStartDate] = useState(new Date());
-    const departments = [
-        'Sales', 'Marketing', 'Engineering', 'Human Resources', 'Legal'
-      ];
-    const [selectedDepartment, setSelectedDepartment] = useState(departments[0]);
-    const states = [
-        {
-            "name": "Alabama",
-            "abbreviation": "AL"
-        },
-        {
-            "name": "Alaska",
-            "abbreviation": "AK"
-        },
-        {
-            "name": "American Samoa",
-            "abbreviation": "AS"
-        },
-        {
-            "name": "Arizona",
-            "abbreviation": "AZ"
-        },
-        {
-            "name": "Arkansas",
-            "abbreviation": "AR"
-        },
-        {
-            "name": "California",
-            "abbreviation": "CA"
-        },
-        {
-            "name": "Colorado",
-            "abbreviation": "CO"
-        },
-        {
-            "name": "Connecticut",
-            "abbreviation": "CT"
-        },
-        {
-            "name": "Delaware",
-            "abbreviation": "DE"
-        },
-        {
-            "name": "District Of Columbia",
-            "abbreviation": "DC"
-        },
-        {
-            "name": "Federated States Of Micronesia",
-            "abbreviation": "FM"
-        },
-        {
-            "name": "Florida",
-            "abbreviation": "FL"
-        },
-        {
-            "name": "Georgia",
-            "abbreviation": "GA"
-        },
-        {
-            "name": "Guam",
-            "abbreviation": "GU"
-        },
-        {
-            "name": "Hawaii",
-            "abbreviation": "HI"
-        },
-        {
-            "name": "Idaho",
-            "abbreviation": "ID"
-        },
-        {
-            "name": "Illinois",
-            "abbreviation": "IL"
-        },
-        {
-            "name": "Indiana",
-            "abbreviation": "IN"
-        },
-        {
-            "name": "Iowa",
-            "abbreviation": "IA"
-        },
-        {
-            "name": "Kansas",
-            "abbreviation": "KS"
-        },
-        {
-            "name": "Kentucky",
-            "abbreviation": "KY"
-        },
-        {
-            "name": "Louisiana",
-            "abbreviation": "LA"
-        },
-        {
-            "name": "Maine",
-            "abbreviation": "ME"
-        },
-        {
-            "name": "Marshall Islands",
-            "abbreviation": "MH"
-        },
-        {
-            "name": "Maryland",
-            "abbreviation": "MD"
-        },
-        {
-            "name": "Massachusetts",
-            "abbreviation": "MA"
-        },
-        {
-            "name": "Michigan",
-            "abbreviation": "MI"
-        },
-        {
-            "name": "Minnesota",
-            "abbreviation": "MN"
-        },
-        {
-            "name": "Mississippi",
-            "abbreviation": "MS"
-        },
-        {
-            "name": "Missouri",
-            "abbreviation": "MO"
-        },
-        {
-            "name": "Montana",
-            "abbreviation": "MT"
-        },
-        {
-            "name": "Nebraska",
-            "abbreviation": "NE"
-        },
-        {
-            "name": "Nevada",
-            "abbreviation": "NV"
-        },
-        {
-            "name": "New Hampshire",
-            "abbreviation": "NH"
-        },
-        {
-            "name": "New Jersey",
-            "abbreviation": "NJ"
-        },
-        {
-            "name": "New Mexico",
-            "abbreviation": "NM"
-        },
-        {
-            "name": "New York",
-            "abbreviation": "NY"
-        },
-        {
-            "name": "North Carolina",
-            "abbreviation": "NC"
-        },
-        {
-            "name": "North Dakota",
-            "abbreviation": "ND"
-        },
-        {
-            "name": "Northern Mariana Islands",
-            "abbreviation": "MP"
-        },
-        {
-            "name": "Ohio",
-            "abbreviation": "OH"
-        },
-        {
-            "name": "Oklahoma",
-            "abbreviation": "OK"
-        },
-        {
-            "name": "Oregon",
-            "abbreviation": "OR"
-        },
-        {
-            "name": "Palau",
-            "abbreviation": "PW"
-        },
-        {
-            "name": "Pennsylvania",
-            "abbreviation": "PA"
-        },
-        {
-            "name": "Puerto Rico",
-            "abbreviation": "PR"
-        },
-        {
-            "name": "Rhode Island",
-            "abbreviation": "RI"
-        },
-        {
-            "name": "South Carolina",
-            "abbreviation": "SC"
-        },
-        {
-            "name": "South Dakota",
-            "abbreviation": "SD"
-        },
-        {
-            "name": "Tennessee",
-            "abbreviation": "TN"
-        },
-        {
-            "name": "Texas",
-            "abbreviation": "TX"
-        },
-        {
-            "name": "Utah",
-            "abbreviation": "UT"
-        },
-        {
-            "name": "Vermont",
-            "abbreviation": "VT"
-        },
-        {
-            "name": "Virgin Islands",
-            "abbreviation": "VI"
-        },
-        {
-            "name": "Virginia",
-            "abbreviation": "VA"
-        },
-        {
-            "name": "Washington",
-            "abbreviation": "WA"
-        },
-        {
-            "name": "West Virginia",
-            "abbreviation": "WV"
-        },
-        {
-            "name": "Wisconsin",
-            "abbreviation": "WI"
-        },
-        {
-            "name": "Wyoming",
-            "abbreviation": "WY"
-        }
+  return (
+    <div>
+      <div className="title">
+        <h1>HRnet</h1>
+      </div>
+      <div className="container">
+        {/* lien vers le composant EmployeeList */}
+        <Link to="/EmployeeList">View Current Employees</Link>
+        <h2>Create Employee</h2>
+        <form action="#" id="create-employee">
+          <label htmlFor="firstName">First Name</label>
+          <input type="text" id="firstName" />
 
-    ];
-    const [selectedState, setSelectedState] = useState(states[0].name);
-async function validEmployee(){
-        // recupere les value des input
-        const firstName = document.querySelector("#firstName").value;
-        const lastName = document.querySelector("#lastName").value;
-        const dateBirth = document.querySelector("#dateBirth").value;
-        const startDate = document.querySelector("#startDate").value;
-        const street = document.querySelector("#street").value;
-        const stateCode = selectedState;
-        const city = document.querySelector("#city").value;
-        const zipCode = document.querySelector("#zipCode").value;
-        const department=selectedDepartment;
-        
-        // payload de l'action
-        const newEmployeeInfo={
-        firstName : firstName,
-        lastName : lastName,
-        startDate : startDate,
-        department :department,
-        dateBirth : dateBirth,
-        street : street,
-        city : city,
-        stateCode :stateCode,
-        zipCode : zipCode,
-        }
-       // appel de la function use dispatch avec en paramètre l'action et le nouvel employé
-       dispatch(addEmployee(newEmployeeInfo))
-        }
-//recupere l'option de department        
-function onSelectDepartment(option){
-    const seletedOption= {selected: option}
-    setSelectedDepartment(seletedOption.selected.value)
-}
-//recupere l'option de State
-function onSelectState(option){
-const selecState={selected: option}
-setSelectedState(selecState.selected.value)
-}
+          <label htmlFor="lastName">Last Name</label>
+          <input type="text" id="lastName" />
 
-    return <div>
-<div className="title">
-            <h1>HRnet</h1>
-        </div>
-        <div className="container">
-            <Link to="/EmployeeList">View Current Employees</Link>
-            <h2>Create Employee</h2>
-            <form action="#" id="create-employee">
-                <label htmlFor="firstName">First Name</label>
-                <input type="text" id="firstName" />
+          <label htmlFor="dateBirth">Date of Birth</label>
+          {/* plugin formulaire de date */}
+          <DatePicker
+            id="dateBirth"
+            selected={startDate}
+            onChange={(date) => setStartDate(date)}
+          />
 
-                <label htmlFor="lastName">Last Name</label>
-                <input type="text" id="lastName" />
+          <label htmlFor="startDate">Start Date</label>
+          {/* plugin formulaire de date */}
+          <DatePicker
+            id="startDate"
+            selected={startDate}
+            onChange={(date) => setStartDate(date)}
+          />
 
-                <label htmlFor="dateBirth">Date of Birth</label>
-                {/* plugin formulaire de date */}
-                <DatePicker id="dateBirth" selected={startDate} onChange={(date) => setStartDate(date)} />
+          <fieldset className="address">
+            <legend>Address</legend>
 
-                <label htmlFor="startDate">Start Date</label>
-                {/* plugin formulaire de date */}
-                <DatePicker id="startDate" selected={startDate} onChange={(date) => setStartDate(date)} />
+            <label htmlFor="street">Street</label>
+            <input id="street" type="text" />
 
-                <fieldset className="address">
-                    <legend>Address</legend>
+            <label htmlFor="city">City</label>
+            <input id="city" type="text" />
 
-                    <label htmlFor="street">Street</label>
-                    <input id="street" type="text" />
+            <label htmlFor="stateCode">State</label>
+            {/* plugin menu déroulant */}
+            <Dropdown
+              placeholder="Select an option"
+              value={selectedState}
+              options={states.map((state) => state.name)}
+              onChange={onSelectState}
+              name="stateCode"
+              id="stateCode"
+            />
 
-                    <label htmlFor="city">City</label>
-                    <input id="city" type="text" />
+            <label htmlFor="zipCode">Zip Code</label>
+            <input id="zipCode" type="number" />
+          </fieldset>
 
-                    <label htmlFor="stateCode">State</label>
-                    {/* plugin menu déroulant */}
-                    <Dropdown  placeholder="Select an option"  value={selectedState} options={states.map(state=>state.name)} onChange={onSelectState }  name="stateCode" id="stateCode"/>
-
-                    <label htmlFor="zipCode">Zip Code</label>
-                    <input id="zipCode" type="number" />
-                </fieldset>
-
-                <label htmlFor="department">Department</label>
-                 {/* plugin menu déroulant */}
-                <Dropdown  placeholder="Select an option"  value={selectedDepartment} options={departments} onChange={onSelectDepartment}  name="department" id="department"/>
-
-            </form>
-
-    
-    <button  onClick={() => {
-        //declenche l'ouverture de la modal
-          openModal();
-          validEmployee();
-        }}>Save</button>
-   
-        <form>
+          <label htmlFor="department">Department</label>
+          {/* plugin menu déroulant */}
+          <Dropdown
+            placeholder="Select an option"
+            value={selectedDepartment}
+            options={departments}
+            onChange={onSelectDepartment}
+            name="department"
+            id="department"
+          />
         </form>
-        </div>
-{/* appel de la library simple modal */}
-<SimpleModal text="Employee created !" options={{width:'600px', maskBgColor: 'rgba(91, 91, 101, 0.5)'}}></SimpleModal>
 
+        <button
+          onClick={() => {
+            //declenche la function de l'ouverture de la modal 
+            openModal();
+            //  declenche la function validEmployee() qui permet de valider et d'enregistrer l'employe
+            validEmployee();
+          }}
+        >
+          Save
+        </button>
 
+        <form></form>
+      </div>
+      {/* appel de la library simple modal */}
+      <SimpleModal
+        text="Employee created !"
+        options={{ width: "600px", maskBgColor: "rgba(91, 91, 101, 0.5)" }}
+      ></SimpleModal>
     </div>
+  );
 }
